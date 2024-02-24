76501a85425a2e26f3e851962ec9d74f049326b7
Louissosthenes9 <Louissosthenes9@gmail.com>
2024-02-24 16:20:07 +0300

    added navitems images

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { PRODUCT_CATEGORIES } from '@/config';
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import NavItem from './NavItem';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
 
 export default function NavItems() {
   const  [activeindex,setActiveIndex] = useState<null | number>(null);
   const isAnyOpen= activeindex !== null;
+  const navRef = useRef<HTMLDivElement | null>(null);
+  useOnClickOutside(navRef,()=>setActiveIndex(null))
+
   return(
-    <div className='flex gap-4 h-full '>
+    <div className='flex gap-4 h-full ' ref={navRef}>
       {PRODUCT_CATEGORIES.map((category,i)=>{
          const handleOpen = ()=>{
            if(activeindex===i){
