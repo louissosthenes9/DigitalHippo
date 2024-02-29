ad3dfa1e0ff48a8d93d2dcf069f4c3c41972b8db
Louissosthenes9 <Louissosthenes9@gmail.com>
2024-02-29 18:19:58 +0300

    added signup

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,14 +1,28 @@
 "use client";
 import { PRODUCT_CATEGORIES } from '@/config';
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import NavItem from './NavItem';
 import { useOnClickOutside } from '@/hooks/use-on-click-outside';
+import { handler } from 'tailwindcss-animate';
 
 export default function NavItems() {
   const  [activeindex,setActiveIndex] = useState<null | number>(null);
   const isAnyOpen= activeindex !== null;
   const navRef = useRef<HTMLDivElement | null>(null);
-  useOnClickOutside(navRef,()=>setActiveIndex(null))
+  useOnClickOutside(navRef,()=>setActiveIndex(null));
+  useEffect(()=>{
+              const handler=(e: KeyboardEvent)=>{
+                if(e.key==="Escape"){
+                  setActiveIndex(null)
+                }
+              }
+              document.addEventListener("keydown",
+                 handler
+             )
+             return ()=>{
+              document.removeEventListener("keydown",handler)
+             }
+  },[])
 
   return(
     <div className='flex gap-4 h-full ' ref={navRef}>
@@ -35,3 +49,4 @@ export default function NavItems() {
   )
   
 }
+ 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,81 @@
-"use client"
-import React from 'react'
-import { MaxWidthWrapper } from './MaxWidthWrapper'
-import Link from 'next/link'
-import { Icons } from './Icons'
-import NavItems from './NavItems'
+"use client";
+import React from "react";
+import { MaxWidthWrapper } from "./MaxWidthWrapper";
+import Link from "next/link";
+import { Icons } from "./Icons";
+import NavItems from "./NavItems";
+import { buttonVariants } from "./ui/button";
 export default function () {
+  const user = null;
   return (
-    <nav className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
-            <header className='bg-white relative'>
-                <MaxWidthWrapper>
-                    <div className='border-b border-gray-200'>
-                        <div className='flex h-16 items-center'>
-                            {/* mobile navbar */}
-                            <div className="ml:4 lg:ml:0 flex">
-                                <Link href="/"><Icons.logo className ="w-10  h-10"/></Link>
-                            </div>
-                            <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
-                               <NavItems />
-                            </div>
+    <nav className="bg-white sticky z-50 top-0 inset-x-0 h-16">
+      <header className="bg-white relative">
+        <MaxWidthWrapper>
+          <div className="border-b border-gray-200">
+            <div className="flex h-16 items-center">
+              {/* mobile navbar */}
+
+              <div className="ml:4 lg:ml:0 flex">
+                <Link href="/">
+                  <Icons.logo className="w-10  h-10" />
+                </Link>
+              </div>
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
+              </div>
+
+              <div className="ml-auto flex items-center">
+                <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                  {user ? null : (
+                    <Link
+                      href={"/sign in"}
+                      className={buttonVariants({
+                        variant: "ghost",
+                      })}
+                    >
+                      Sign in
+                    </Link>
+                  )}
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+
+                  {user ? (
+                    <p></p>
+                  ) : (
+                    <Link
+                      href="/register"
+                      className={buttonVariants({
+                        variant: "ghost",
+                      })}
+                    >
+                      Create account
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span className="h-6 w-px bg-gray-200" 
+                    aria-hidden="true" />
+                  ):null}
+
+                  {
+                    user?null:(
+                        <div className='flex lg:ml-6'>
+                                 <span className="h-6 w-px bg-gray-200" 
+                                 aria-hidden="true" />
                         </div>
-                    </div>
-                </MaxWidthWrapper>
-            </header>
+                    )
+                  }
+
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
     </nav>
-  )
+  );
 }
