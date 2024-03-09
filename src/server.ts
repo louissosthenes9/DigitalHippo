32fc6848e20ae57bac0fda9ef35b8a5be14fd6c8
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-09 17:48:58 +0300

    finished configuring payload with mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { getPayloadClient } from "./get-payload";
+import { Nexthandler, nextApp } from "./next-utils";
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -12,4 +13,14 @@ const start =  async ()=>{
            }
         }
     })
+
+    app.use((req,res)=>Nexthandler(req,res))
+    nextApp.prepare().then(()=>{
+     // payload.logger.info("Nextjs started")
+      app.listen(PORT, async () => {
+        // payload.logger.info(`Next.js App URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+      })
+    })
 }
+
+start()
