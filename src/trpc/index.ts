f2935180ecd109a5112a0c4739cf772413350c4a
loussosthenes9 <louissosthenes9@gmail.com>
2024-03-15 16:30:15 +0300

    fixed a bug

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,6 +1,10 @@
-import { router } from "./trpc";
+import { publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
+    anyApiroute:publicProcedure.query(()=>{
+        return "hellow world"
+    }
+    ),
 })
 
 export type Approuter = typeof appRouter  
