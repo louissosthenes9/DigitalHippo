02cdd388baf9219aad9110964df61631e67c7f2c
louissosthenes9 <louissosthenes9@gmail.com>
2024-03-28 23:39:34 +0300

    verification-email

---
+++ src/app/(auth)/verify-email/page.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Image from 'next/image'
+
+interface PageProps {
+  searchParams: { [key: string]: string[] | undefined }
+}
+
+export default function page({ searchParams }: PageProps) {
+  const token = searchParams.token
+  const toEmail = searchParams.to
+
+  return (
+    <div className='container relative flex flex-col pt-20 items-center lg:px-0 justify-center '>
+      <div className="mx-auto flex flex-col w-full space-y-6 sm:w-[350px]">
+        {token && typeof token === "string" ? (
+          <div className="grid gap-6">
+            {/* Add your content for the token case here */}
+          </div>
+        ) : (
+          <div className="flex flex-col space-y-1 h-full justify-center items-center">
+            <div className="relative mb-4 w-60 h-60 text-muted-foreground">
+              <Image
+                src="/hippo-email-sent.png"
+                width={250}
+                height={250}
+                alt='hippo image sent'
+              />
+              <h3 className="font-semibold text-2xl">
+                check your email
+              </h3>
+              {toEmail ? (
+                <p className='text-muted-foreground text-center'>
+                  We've sent a verification link to <span className='font-semibold'>{toEmail}</span>.
+                </p>
+              ) : (
+                <p>We've sent a verification email.</p>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
