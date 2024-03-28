55e36244cd51f38a160cdf22eb07691954cf9158
louissosthenes9 <louissosthenes9@gmail.com>
2024-03-28 22:56:36 +0300

    fixed 404 bug

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,102 +1,141 @@
-"use client";
-import { Icons } from "@/components/Icons";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { cn } from "@/lib/utils";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { ArrowRight } from "lucide-react";
-import Link from "next/link";
-import React from "react";
+'use client'
+
+import { Icons } from '@/components/Icons'
+import {
+  Button,
+  buttonVariants,
+} from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { cn } from '@/lib/utils'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { ArrowRight } from 'lucide-react'
+import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+
 import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
-} from "@/lib/validators/account-credentials-validator";
-import { trpc } from "@/trpc/client";
-import { toast } from "sonner";
-import { ZodError } from "zod";
-import { useForm } from "react-hook-form";
-const page = () => {
+} from '@/lib/validators/account-credentials-validator'
+import { trpc } from '@/trpc/client'
+import { toast } from 'sonner'
+import { ZodError } from 'zod'
+import { useRouter } from 'next/navigation'
+
+const Page = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<TAuthCredentialsValidator>({
     resolver: zodResolver(AuthCredentialsValidator),
-  });
-  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
-    mutate({
-      email,
-      password,
-    });
-  };
-  const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({});
+  })
+
+  const router = useRouter()
+
+  const { mutate, isLoading } =
+    trpc.auth.createPayloadUser.useMutation({
+      onError: (err) => {
+        if (err.data?.code === 'CONFLICT') {
+          toast.error(
+            'This email is already in use. Sign in instead?'
+          )
+
+          return
+        }
+
+        if (err instanceof ZodError) {
+          toast.error(err.issues[0].message)
+
+          return
+        }
+
+        toast.error(
+          'Something went wrong. Please try again.'
+        )
+      },
+      onSuccess: ({ sentToEmail }) => {
+        toast.success(
+          `Verification email sent to ${sentToEmail}.`
+        )
+        router.push('/verify-email?to=' + sentToEmail)
+      },
+    })
+
+  const onSubmit = ({
+    email,
+    password,
+  }: TAuthCredentialsValidator) => {
+    mutate({ email, password })
+  }
 
   return (
     <>
-      <div className="container relative flex flex-col items-center justify-center lg:px-0 pt-20">
-        {/*logo and heading of the signup page */}
-        <div className="mx-auto flex w-full flex-col justify-center items-center space-y-6 sm:w-[350px]">
-          <div className="text-center flex flex-col space-y-2 justify-center">
-            <Icons.logo className="h-20 w-20 mx-auto" />
-            <h1 className="text-2xl font-bold">create an account</h1>
+      <div className='container relative flex pt-20 flex-col items-center justify-center lg:px-0'>
+        <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
+          <div className='flex flex-col items-center space-y-2 text-center'>
+            <Icons.logo className='h-20 w-20' />
+            <h1 className='text-2xl font-semibold tracking-tight'>
+              Create an account
+            </h1>
+
             <Link
               className={buttonVariants({
-                variant: "link",
-                className: "gap-1.5",
+                variant: 'link',
+                className: 'gap-1.5',
               })}
-              href="/sign-up"
-            >
-              Already have an account? sign-in
-              <ArrowRight className="h-4 w-4" />
+              href='/sign-in'>
+              Already have an account? Sign-in
+              <ArrowRight className='h-4 w-4' />
             </Link>
           </div>
 
-          {/* sign up form */}
-          <div className="grid gap-6">
+          <div className='grid gap-6'>
             <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="grid gap-2">
-                <div className="grid gap-1 py-2">
-                  <Label htmlFor="email">Email</Label>
+              <div className='grid gap-2'>
+                <div className='grid gap-1 py-2'>
+                  <Label htmlFor='email'>Email</Label>
                   <Input
-                    {...register("email")}
+                    {...register('email')}
                     className={cn({
-                      "focus-visible-ring-red-500": errors.email,
+                      'focus-visible:ring-red-500':
+                        errors.email,
                     })}
-                    type="email"
-                    placeholder="you@email.com"
+                    placeholder='you@example.com'
                   />
-
-                  {errors?.password && (
-                    <p className="text-sm text-red-500">
-                      {errors.password.message}
+                  {errors?.email && (
+                    <p className='text-sm text-red-500'>
+                      {errors.email.message}
                     </p>
                   )}
                 </div>
-                <div className="grid gap-1 py-2">
-                  <Label htmlFor="password">Passsword</Label>
+
+                <div className='grid gap-1 py-2'>
+                  <Label htmlFor='password'>Password</Label>
                   <Input
-                    {...register("password")}
+                    {...register('password')}
+                    type='password'
                     className={cn({
-                      "focus-visible-ring-red-500": errors.password,
+                      'focus-visible:ring-red-500':
+                        errors.password,
                     })}
-                    type="password"
-                    placeholder="password"
+                    placeholder='Password'
                   />
-                </div>
-                {errors?.password && (
+                  {errors?.password && (
                     <p className='text-sm text-red-500'>
                       {errors.password.message}
                     </p>
                   )}
-                <Button>sign up</Button>
+                </div>
+
+                <Button>Sign up</Button>
               </div>
             </form>
           </div>
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default page;
+export default Page
