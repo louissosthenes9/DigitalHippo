a90af33835ba124e8e6feeab8aa3aad68d49f97d
louissosthenes9 <louissosthenes9@gmail.com>
2024-03-28 22:57:09 +0300

    added access and auth  fields

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,25 +1,51 @@
+import { PrimaryActionEmailHtml } from '../components/emails/PrimaryActionEmail'
+import { Access, CollectionConfig } from 'payload/types'
 
-import { CollectionConfig } from "payload/types";
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === 'admin') return true
 
-export const Users : CollectionConfig = {
-    slug:"users",
-    auth:true,
-    access:{
-        read:()=>true,
-        create:()=>true,
-
+  return {
+    id: {
+      equals: user.id,
     },
-    fields:[
-        { 
-            name:'role',
-            required:true,
-            defaultValue:'user',
-            type:'select',
-            options:[
-                {label:"Admin",value:"admin"},
-                {label:"User",value:'user'}
-            ]
+  }
+}
 
-        }
-    ]
+export const Users: CollectionConfig = {
+  slug: 'users',
+  auth: {
+    verify: {
+      generateEmailHTML: ({ token }) => {
+        return PrimaryActionEmailHtml({
+          actionLabel: "verify your account",
+          buttonText: "Verify Account",
+          href: `${process.env.NEXT_PUBLIC_SERVER_URL}/verify-email?token=${token}`
+        })
+      },
+    },
+  },
+  fields:[
+    { 
+        name:'role',
+        required:true,
+        defaultValue:'user',
+        type:'select',
+        options:[
+            {label:"Admin",value:"admin"},
+            {label:"User",value:'user'}
+        ]
+    
+    }
+],
+  access: {
+    read: adminsAndUser,
+    create: () => true,
+    update: ({ req }) => req.user.role === 'admin',
+    delete: ({ req }) => req.user.role === 'admin',
+  },
+  admin: {
+    hidden: ({ user }) => user.role !== 'admin',
+    defaultColumns: ['id'],
+  },
+ 
 }
