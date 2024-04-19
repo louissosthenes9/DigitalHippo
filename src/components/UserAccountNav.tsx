e67e9740172251101bdbd719bd0fcaaac11e7c9c
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-19 17:22:02 +0300

    added user collection on top of the navbar

---
+++ src/components/UserAccountNav.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { User } from '@/payload-types'
+import Link from 'next/link'
+import { useAuth } from '@/hooks/use-auth'
+export default function UserAccountNav({user}:{user: User}) {
+ const {signOut}= useAuth()
+ 
+  return (
+    <DropdownMenu>
+       <DropdownMenuTrigger asChild className='overflow-visible'>
+           <Button variant={'ghost'} size='sm' className='relative'>My Account</Button>
+       </DropdownMenuTrigger>
+       <DropdownMenuContent className='bg-white w-60' align="end">
+         <div className="flex items-center justify-start gap-2 p-2">
+           <div className='flex flex-col space-y-0.5 leading-none'>
+              <p className='font-medium text-sm '>{user.email}</p>
+           </div>
+           
+         </div>
+         <DropdownMenuSeparator/>
+         <DropdownMenuItem asChild>
+           <Link href='/sell'>Seller DashBoard</Link>
+         </DropdownMenuItem>
+         <DropdownMenuItem onClick={signOut} className='cursor-pointer'>
+          Log Out 
+         </DropdownMenuItem>
+       </DropdownMenuContent>
+
+    </DropdownMenu>
+  )
+}
