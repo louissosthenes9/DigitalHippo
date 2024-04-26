956cb1b68cbbc9d0d550a3d4fcfead9f175264de
louissosthenes9 <louissosthenes9@gmail.com>
2024-04-26 23:56:39 +0300

    added access policy

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,5 +1,19 @@
-import { CollectionConfig } from "payload/types";
+import {User} from "../payload-types"
+import { Access, CollectionConfig } from "payload/types";
 
+const isAdminorHasAccessToImages=():Access=> 
+async({req,})=>{
+   const user = req.user as User | undefined
+
+   if(!user) return false;
+   if(user.role==='admin') return true
+
+   return{
+    user:{
+        equals:req.user.id
+    }
+   }
+}
 export const Media :CollectionConfig ={
     slug:"media",
     hooks:{
@@ -9,6 +23,20 @@ export const Media :CollectionConfig ={
             },
         ],
     },
+    access:{
+      read:async ({req})=>{
+        const referrer = req.headers.referer
+        if(!req.user || referrer?.includes("sell")){
+             return true
+        }
+
+        return await isAdminorHasAccessToImages()({req})
+      },
+      delete:isAdminorHasAccessToImages(),
+      update:isAdminorHasAccessToImages()
+    },
+   
+    
 
     admin:{
         hidden:({user})=>user.role !=='admin',
