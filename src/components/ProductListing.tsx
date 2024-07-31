8f906fc024f8a0a66835528181d44b4f5b62e222
louissosthenes9 <louissosthenes9@gmail.com>
2024-07-31 15:03:46 +0300

    fixed the type error

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -1,83 +1,63 @@
-import { Product } from '@/payload-types'
-import { useEffect, useState } from 'react'
+import { Product } from '@/payload-types';
+import { useEffect, useState } from 'react';
 import { Skeleton } from './ui/skeleton';
 import Link from 'next/link';
 import { cn, formatPrice } from '@/lib/utils';
 import { PRODUCT_CATEGORIES } from '@/config';
 import ImageSlider from './ImageSlider';
 
-interface ProductListingProps{
-    product:Product | null
-    index:number
+interface ProductListingProps {
+  product: Product | null;
+  index: number;
 }
-export default function ProductListing({product,index}:ProductListingProps) {
-    const validUrls = product?.images.map(
-        ({image})=>(
-          typeof image === "string" ? image : image.url
-        )
-    ).filter(Boolean) as string[]
-    
-    const label = PRODUCT_CATEGORIES.find(
-        ({value})=> value===product?.category
-     )?.label
 
-    const [isVisible,setIsVisible] = useState<boolean>(false);
-    if(!product || !isVisible) return <ProductPlaceholder />
+export default function ProductListing({ product, index }: ProductListingProps) {
+  const validUrls = product?.images
+    .map(({ image }) => (typeof image === 'string' ? image : image.url))
+    .filter(Boolean) as string[];
 
-      useEffect(
-       ()=>{
-            const timer = setTimeout(()=>{
-                setIsVisible(true)
-            },index * 75)
+  const label = PRODUCT_CATEGORIES.find(({ value }) => value === product?.category)?.label;
 
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
-            return ()=>clearTimeout(timer)
-       } ,[index] 
-      )
-    if(isVisible && product){
-        return (
-            <Link 
-            className={cn('invisible h-full w-full cursor-pointer group/main',{
-                "visible animate-in fade-in-0":isVisible,
-            })}
-            href={`/product/${product.id}`}
-            >
-                <ImageSlider urls={validUrls} />
-                <div className="flex flex-col w-full">
-                    <h3 className='mt-4 font-medium text-sm text-gray-700'>
-                        {product.name}
-                    </h3>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, index * 75);
 
-                    <p className="mt-1 text-smt text-gray-500">
-                        {product.category}
-                    </p>
-                    <p className='mt-1 font-medium text-sm text-gray-900'>{formatPrice(product.price)}</p>
-                </div>
-            </Link>
-        )
-    }
+    return () => clearTimeout(timer);
+  }, [index]);
 
-   
-  return ( 
+  if (!product || !isVisible) return <ProductPlaceholder />;
 
-    <div className='flex flex-col w-full'>
-        div
+  console.log(product.price)
+
+  return (
+    <Link
+      className={cn('invisible h-full w-full cursor-pointer group/main', {
+        'visible animate-in fade-in-0': isVisible,
+      })}
+      href={`/product/${product.id}`}
+    >
+      <ImageSlider urls={validUrls} />
+      <div className="flex flex-col w-full">
+        <h3 className="mt-4 font-medium text-sm text-gray-700">{product.name}</h3>
+        <p className="mt-1 text-sm text-gray-500">{label}</p>
+        <p className="mt-1 font-medium text-sm text-gray-900">{formatPrice(product.price)}</p>
+      </div>
+    </Link>
+  );
+}
+
+const ProductPlaceholder = () => {
+  return (
+    <div className="flex flex-col w-full">
+      <div className="relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
+        <Skeleton className="h-full w-full" />
+      </div>
+      <Skeleton className="mt-4 w-2/3 h-4 rounded-lg" />
+      <Skeleton className="mt-2 w-16 h-4 rounded-lg" />
+      <Skeleton className="mt-4 w-12 h-4 rounded-lg" />
     </div>
-
-
-  )
-}
-
-const ProductPlaceholder=()=>{
-    return (
-        <div className="flex flex-col w-full">
-            <div className="relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
-                <Skeleton className='h-full w-full' />
-            </div>
-
-              <Skeleton className='mt-4 w-2/3 h-4 rounded-lg' />
-              <Skeleton className='mt-2 w-16 h-4  rounded-lg' />
-              <Skeleton className='mt-4 w-12  h-4 rounded-lg' />
-        </div>
-    )  
-}
+  );
+};
